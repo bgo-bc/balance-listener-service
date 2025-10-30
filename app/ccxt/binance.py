ba65fd842c71b9b9e5a385b3a8f459805966631b
Benito Go III <bgo@bluecubeservicesltd.com>
2025-10-30 13:54:11 +0800

    Updated defaultType for Binance inverse positions

--- app/ccxt/binance.py
+++ app/ccxt/binance.py
@@ -35,7 +35,7 @@ class BinanceAdapter(BaseAdapter):
             "positions-inverse": {
                 "name": "binancecoinm",
                 "options": {
-                    "defaultType": "future"
+                    "defaultType": "delivery"
                 }
             }
         }
