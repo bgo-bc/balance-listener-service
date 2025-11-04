51de4ff600c2a446d5d433cfcdd32cab0dfcd400
Benito Go III <bgo@bluecubeservicesltd.com>
2025-11-04 17:10:54 +0800

    Trying gateio earn api

--- app/ccxt/deribit.py
+++ app/ccxt/deribit.py
@@ -1,9 +1,10 @@
 from typing import Dict, Any, List, Optional
+import ccxt.pro as ccxt
 from app.ccxt.base import BaseAdapter
 from app.utils.logging import get_logger
 
 
-logger = get_logger("ccxt_binance_adapter")
+logger = get_logger("ccxt_deribit_adapter")
 
 
 class DeribitAdapter(BaseAdapter):
@@ -17,15 +18,12 @@ class DeribitAdapter(BaseAdapter):
         return [positions]
     
     async def fetch_funding_fees(self) -> List[Dict[str, Any]]:
-        exchange = self.exchanges.get("funding_fee") or self.exchanges.get("default")
-        fetch_method = getattr(exchange, "fetch_funding_rate_history")
-        
-        if not fetch_method:
-            logger.info(f"Exchange {self.exchange_id} does not have fetch_funding_rate_history() method")
-            return None
+        exchange: ccxt.deribit = self.exchanges.get("funding_fees") or self.exchanges.get("default")
         
         try:
-            return [await fetch_method(symbol="BTC-PERPETUAL")]
+            response = await exchange.private_get_get_settlement_history_by_currency(params={"currency": "BTC"})
+            result = response["result"]
+            return result["settlements"]
         except Exception as e:
             logger.error(f"fetch_funding_fees() error: {e}")
-            return None
+            return []
--- app/ccxt/gateio.py
+++ app/ccxt/gateio.py
@@ -4,13 +4,21 @@ from app.ccxt.base import BaseAdapter
 from app.utils.logging import get_logger
 
 
-logger = get_logger("ccxt_binance_adapter")
+logger = get_logger("ccxt_gateio_adapter")
 
 
 class GateioAdapter(BaseAdapter):
     def __init__(self, exchange_id: str, credentials: Optional[dict[str, str]] = None):
         super().__init__(exchange_id, credentials)
     
+    async def fetch_earn_balance(self) -> List[Dict[str, Any]]:
+        exchange: ccxt.gateio = self.exchanges.get("default")
+        try:
+            return await exchange.private_earn_get_uni_lends()
+        except Exception as e:
+            logger.error(f"fetch_earn_balance() error: {e}")
+            return []
+
     async def fetch_funding_fees(self) -> List[Dict[str, Any]]:
         exchange: ccxt.gateio = self.exchanges.get("funding_fees") or self.exchanges.get("default")
         fetch_method = getattr(exchange, "fetch_funding_history")
